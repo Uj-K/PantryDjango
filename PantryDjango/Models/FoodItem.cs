b607acfec8bb326e9251ee11dfa175888d3aa93b
Uj-K <umkrall@gmail.com>
2025-04-13 23:22:01 +0900

    Add more details

--- PantryDjango/Models/FoodItem.cs
+++ PantryDjango/Models/FoodItem.cs
@@ -13,22 +13,25 @@ namespace PantryDjango.Models
         public int Id { get; set; }
 
         // name of the food item  
+        [Required]
+        [StringLength(100)]
         public required string Name { get; set; }
 
         // optional description of the food item  
+        [StringLength(500)]
         public string? Description { get; set; }
 
         // date when the food will be expired  
         public DateTime ExpirationDate { get; set; }
 
         // number of items if the food item is more than one  
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be a positive number.")]
         public int Quantity { get; set; }
 
         // unit of measurement for the food item (e.g., kg, g, L, mL, etc.)  
+        [RegularExpression("kg|g|L|mL|pcs", ErrorMessage = "Unit must be one of the following: kg, g, L, mL, pcs.")]
         public string? Unit { get; set; }
 
-        // category of the food item (e.g., Dairy, Meat, Vegetable, etc.)  
-        public required string Category { get; set; }
 
         // date when the food item was added to the pantry or fridge  
         public DateOnly AddedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
@@ -36,7 +39,29 @@ namespace PantryDjango.Models
         // date when the food item was last updated  
         public DateOnly UpdatedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 
-        // location where the food item is stored (e.g., pantry, fridge, freezer, etc.)  
-        public string? location { get; set; }
+        // food category 
+        public enum FoodCategory
+        {
+            Dairy,
+            Meat,
+            Vegetable,
+            Fruit,
+            Grain,
+            Other
+        }
+
+        public FoodCategory Category { get; set; }
+
+        // location of the food item 
+        public enum StorageLocation
+        {
+            Pantry,
+            Fridge,
+            Freezer,
+            Other
+        }
+
+        public StorageLocation? Location { get; set; }
+
     }
 }
