44857c04d2215e074947d70db9f6e9b010cb31e6
Uj-K <umkrall@gmail.com>
2025-04-14 21:31:25 +0900

    Update program.cs. I accidentally created two paths, which caused error

--- PantryDjango/Program.cs
+++ PantryDjango/Program.cs
@@ -5,17 +5,15 @@ using PantryDjango.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<FoodItemContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<FoodItemContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("PantryConnection")));
 
 
 var app = builder.Build();
