595b0645c35cbc2b77b59ed11dc2562edee1feb6
Uj-K <umkrall@gmail.com>
2025-04-22 14:59:53 +0900

    Merge FoodItemsContext into ApplicationDbContext
    (class 나 키워드 선택하고 F12를 누르면 자세한 definition 을 볼 수 있다.)

--- PantryDjango/Data/ApplicationDbContext.cs
+++ PantryDjango/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PantryDjango.Models;
 
 namespace PantryDjango.Data
 {
@@ -9,5 +10,7 @@ namespace PantryDjango.Data
             : base(options)
         {
         }
+
+        public DbSet<FoodItem> FoodItems { get; set; }
     }
 }
