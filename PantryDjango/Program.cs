0b540be6154178c001e031710e566de02f03a377
Uj-K <umkrall@gmail.com>
2025-04-27 22:47:11 +0900

    Update the first page to food list

--- PantryDjango/Program.cs
+++ PantryDjango/Program.cs
@@ -39,7 +39,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=FoodItems}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.MapRazorPages()
